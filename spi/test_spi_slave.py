c89eb6015fe32c8d705394e49f81c0c9a729584f
mngr0 <ngrmarco@libero.it>
2018-08-22 19:46:39 +0200

    work on spi

--- spi/test_spi_slave.py
+++ spi/test_spi_slave.py
@@ -10,14 +10,14 @@ miso = Signal(0)
 cs = Signal(1)
 out_bute = Signal(intbv(0)[8:])
 in_byte = Signal(intbv(0)[8:])
-
+enable = Signal(0)
 
 @block
 def test_ss(clock, mosi, cs, out):
 	clkdrv = ClkDriver(clock,period=10)
 	ss = spi_slave (clock, miso, mosi, cs, out_byte)
-	tser = ser(clock,in_byte,mosi,)
-	return clkdrv, ss
+	tser = ser(clock,in_byte,mosi,enable )
+	return clkdrv, ss, tser
 
 tr = test_ss(clock, mosi, cs, out_byte)
 tr.config_sim(trace=True)
